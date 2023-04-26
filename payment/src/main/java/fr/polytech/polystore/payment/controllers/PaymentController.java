bef6d730198d5508f2897890497d68f32ea19e25
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-04-26 19:54:12 +0200

    feat(payment): rest controller
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- payment/src/main/java/fr/polytech/polystore/payment/controllers/PaymentController.java
+++ payment/src/main/java/fr/polytech/polystore/payment/controllers/PaymentController.java
@@ -1,14 +1,26 @@
 package fr.polytech.polystore.payment.controllers;
 
+import fr.polytech.polystore.common.dtos.PaymentDTO;
 import fr.polytech.polystore.payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/payment")
+@RequestMapping("/api/v1/payments")
 public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
+
+    @GetMapping()
+    public ResponseEntity<List<PaymentDTO>> getAllPayments() {
+        List<PaymentDTO> payments = this.paymentService.getAllPayments();
+        return ResponseEntity.ok(payments);
+    }
 }
